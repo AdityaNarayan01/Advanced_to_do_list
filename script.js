4938812f9a576cd3e2c3cdd369e740ea8ccff079
AdityaNarayan01 <aditya106901@gmail.com>
2022-04-05 00:49:52 +0530

    change priority color

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let modalCont = document.querySelector(".model-cont");
 
 let addFlag = false;
 
+let colors = ["lightpink", "lightblue", "lightgreen", "black"];
+let modalPriorityColor = colors[colors.length - 1]; //black
+
+let allPriorityColors = document.querySelectorAll(".priority-color");
+
+let mainCont = document.querySelector(".main-cont");
+
 addbutton.addEventListener("click", function (e) {
   //display a model
 
@@ -18,3 +25,38 @@ addbutton.addEventListener("click", function (e) {
     modalCont.style.display = "none";
   }
 });
+
+//Change in priority colors
+
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function (e) {
+    allPriorityColors.forEach(function (priorityColorElem) {
+      priorityColorElem.classList.remove("active");
+    });
+    colorElem.classList.add("active");
+
+    modalPriorityColor = colorElem.classList[0];
+  });
+});
+
+//generating a ticket
+modalCont.addEventListener("keydown", function (e) {
+  let key = e.key;
+
+  if (key == "Shift") {
+    createTicket(); //this function will generate the ticket
+    modalCont.style.display = "none";
+    addFlag = false;
+  }
+});
+
+function createTicket() {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+
+  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  <div class="ticket-id"></div>
+  <div class="ticket-area"></div>`;
+
+  mainCont.appendChild(ticketCont);
+}
